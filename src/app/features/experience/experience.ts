a7430c6b0ec9d9d940049cec2ef3ba69e9c332bc
Gabriele <gabrielecabriniii@gmail.com>
2026-07-11 00:20:08 +0200

    fix: remove education from experiences

--- src/app/features/experience/experience.ts
+++ src/app/features/experience/experience.ts
@@ -2,11 +2,10 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { I18nService } from '../../core/services/i18n.service';
-import { WORK_EXPERIENCES, SKILLS, CERTIFICATIONS, LANGUAGES, EDUCATION } from '../../core/data/experience';
+import { WORK_EXPERIENCES, SKILLS, CERTIFICATIONS, LANGUAGES } from '../../core/data/experience';
 import { WorkExperience } from '../../core/models/work-experience.model';
 import { Skill } from '../../core/models/skill.model';
 import { Certification } from '../../core/models/certification.model';
-import { Education } from '../../core/models/education.model';
 import { DateFormatPipe } from '../../core/pipes/date-format.pipe';
 import { DateRangePipe } from '../../core/pipes/date-range.pipe';
 
@@ -20,7 +19,6 @@ import { DateRangePipe } from '../../core/pipes/date-range.pipe';
 export class Experience {
   readonly i18n = inject(I18nService);
   readonly workExperiences: WorkExperience[] = WORK_EXPERIENCES;
-  readonly education: Education[] = EDUCATION;
   readonly skills: Skill[] = SKILLS;
   readonly languages: Skill[] = LANGUAGES;
   readonly certifications: Certification[] = CERTIFICATIONS;
