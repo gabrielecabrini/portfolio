f68c2bcbf2673f6f7a30fe8081ce7fc19f363ee8
Gabriele <gabrielecabriniii@gmail.com>
2026-07-11 23:55:34 +0200

    feat: add subtitle typewriter

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { afterNextRender, ChangeDetectionStrategy, Component, OnDestroy, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
@@ -9,4 +9,49 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './home.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {}
+export class Home implements OnDestroy {
+  readonly displayText = signal('');
+
+  private readonly phrases = [
+    'Fullstack Software Developer',
+    'Spring Boot & Kotlin Engineer',
+    'Angular Developer',
+    'Linux & Open Source Enthusiast',
+  ];
+  private phraseIndex = 0;
+  private charIndex = 0;
+  private deleting = false;
+  private timer: ReturnType<typeof setTimeout> | null = null;
+
+  constructor() {
+    afterNextRender(() => this.tick());
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) clearTimeout(this.timer);
+  }
+
+  private tick(): void {
+    const phrase = this.phrases[this.phraseIndex];
+
+    if (!this.deleting) {
+      this.charIndex++;
+      this.displayText.set(phrase.slice(0, this.charIndex));
+      if (this.charIndex === phrase.length) {
+        this.timer = setTimeout(() => { this.deleting = true; this.tick(); }, 1800);
+        return;
+      }
+    } else {
+      this.charIndex--;
+      this.displayText.set(phrase.slice(0, this.charIndex));
+      if (this.charIndex === 0) {
+        this.deleting = false;
+        this.phraseIndex = (this.phraseIndex + 1) % this.phrases.length;
+        this.timer = setTimeout(() => this.tick(), 400);
+        return;
+      }
+    }
+
+    this.timer = setTimeout(() => this.tick(), this.deleting ? 40 : 80);
+  }
+}
